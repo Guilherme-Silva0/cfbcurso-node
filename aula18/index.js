df54ccf3239765f44605aa3c370a2e7b1b53cbb0
Guilherme Silva <luiz.silva136@aluno.ce.gov.br>
2023-03-07 16:59:40 -0300

    Contando registros inseridos e deletados

--- aula18/index.js
+++ aula18/index.js
@@ -6,12 +6,24 @@ const url = `mongodb+srv://${process.env.MONGODB_USER}:${process.env.MONGODB_PAS
 mongodb.connect(url, { useNewUrlParser: true }, (err, banco) => {
   if (err) throw err;
   const dbo = banco.db("teste-node");
-  const obj = { nome: "Natália", idade: 33 };
+  // const obj = { nome: "Natália", idade: 33 };
 
   // dbo.collection("usuarios").insertOne(obj, (err, res) => {
   //   if (err) throw err;
   //   console.log(`Nova linha adicionada`);
   //   // banco.close();
+  // });
+
+  // const obj = [
+  //   { nome: "Fernanda", idade: 34 },
+  //   { nome: "Mathias", idade: 12 },
+  //   { nome: "João", idade: 14 },
+  //   { nome: "Priscila", idade: 2 },
+  // ];
+  // dbo.collection("usuarios").insertMany(obj, async (err, res) => {
+  //   if (err) throw err;
+  //   await console.log(res.insertedCount + " nova(s) linha(s) adicionada(s)");
+  //   banco.close();
   // });
 
   // dbo
@@ -23,15 +35,15 @@ mongodb.connect(url, { useNewUrlParser: true }, (err, banco) => {
   //     banco.close();
   //   });
 
-  // const query = {};
-  // dbo
-  //   .collection("usuarios")
-  //   .find(query, { projection: { _id: 0 } })
-  //   .toArray((err, res) => {
-  //     if (err) throw err;
-  //     console.log(res);
-  //     banco.close();
-  //   });
+  const query = {};
+  dbo
+    .collection("usuarios")
+    .find(query, { projection: { _id: 0 } })
+    .toArray((err, res) => {
+      if (err) throw err;
+      console.log(res);
+      banco.close();
+    });
 
   // const ordenacao = { idade: 1 };
   // const query = {};
@@ -46,11 +58,11 @@ mongodb.connect(url, { useNewUrlParser: true }, (err, banco) => {
   //     banco.close();
   //   });
 
-  const query = { nome: "Fernanda" };
+  // const query = { nome: "Fernanda" };
 
-  dbo.collection("usuarios").deleteOne(query, (err, _) => {
-    if (err) throw err;
-    console.log("Linha deletada");
-    banco.close();
-  });
+  // dbo.collection("usuarios").deleteOne(query, (err, _) => {
+  //   if (err) throw err;
+  //   console.log("Linha deletada");
+  //   banco.close();
+  // });
 });
