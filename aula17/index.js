0e7304bcb82ed39a5bef7b5b6a72697401147b2a
Guilherme Silva <luiz.silva136@aluno.ce.gov.br>
2023-03-07 16:03:16 -0300

    iniciando aula17

---
+++ aula17/index.js
@@ -0,0 +1,48 @@
+const mongodb = require("mongodb").MongoClient;
+const dotenv = require("dotenv");
+dotenv.config();
+const url = `mongodb+srv://${process.env.MONGODB_USER}:${process.env.MONGODB_PASSWORD}@cluster0.omwkdsg.mongodb.net/?retryWrites=true&w=majority`;
+
+mongodb.connect(url, { useNewUrlParser: true }, (err, banco) => {
+  if (err) throw err;
+  const dbo = banco.db("teste-node");
+  const obj = { nome: "Natália", idade: 33 };
+
+  // dbo.collection("usuarios").insertOne(obj, (err, res) => {
+  //   if (err) throw err;
+  //   console.log(`Nova linha adicionada`);
+  //   // banco.close();
+  // });
+
+  // dbo
+  //   .collection("usuarios")
+  //   .find({}, { projection: { _id: 0 } })
+  //   .toArray((err, res) => {
+  //     if (err) throw err;
+  //     console.log(res[0].nome);
+  //     banco.close();
+  //   });
+
+  // const query = {};
+  // dbo
+  //   .collection("usuarios")
+  //   .find(query, { projection: { _id: 0 } })
+  //   .toArray((err, res) => {
+  //     if (err) throw err;
+  //     console.log(res);
+  //     banco.close();
+  //   });
+
+  const ordenacao = { idade: 1 };
+  const query = {};
+
+  dbo
+    .collection("usuarios")
+    .find(query)
+    .sort(ordenacao)
+    .toArray((err, res) => {
+      if (err) throw err;
+      console.log(res);
+      banco.close();
+    });
+});
